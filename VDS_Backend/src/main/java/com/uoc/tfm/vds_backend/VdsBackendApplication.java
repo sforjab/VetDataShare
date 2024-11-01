d0f38c339b8dfe21e613547b8bbd9ff333167e3b
Sergio Forja Barriga <sergio.forja88@gmail.com>
2024-11-01 15:22:13 +0100

    Creación del proyecto para backend. Se añaden dependencias Maven

---
+++ VDS_Backend/src/main/java/com/uoc/tfm/vds_backend/VdsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uoc.tfm.vds_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdsBackendApplication.class, args);
+	}
+
+}
---
+++ VDS_Backend/src/test/java/com/uoc/tfm/vds_backend/VdsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uoc.tfm.vds_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VdsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
