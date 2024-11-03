a284685edc91f5222eb867add71eda1d00b8461a
Sergio Forja Barriga <sergio.forja88@gmail.com>
2024-11-03 13:07:14 +0100

    Actualiación de app-routing

--- VDS_Frontend/src/app/app-routing.module.ts
+++ VDS_Frontend/src/app/app-routing.module.ts
@@ -11,6 +11,24 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) 
   },
+  {
+    path: 'cliente', 
+    loadChildren: () => import('./usuarios/cliente/cliente.module').then(m => m.ClienteModule),
+  },
+  {
+    path: 'veterinario', 
+    loadChildren: () => import('./usuarios/veterinario/veterinario.module').then(m => m.VeterinarioModule),
+  },
+  /* {
+    path: 'admin', 
+    loadChildren: () => import('./usuarios/admin/admin.module').then(m => m.AdminModule),
+  }, */
+  {
+    path: 'mascota', 
+    loadChildren: () => import('./mascota/mascota.module').then(m => m.MascotaModule),
+    /* canActivate: [AuthGuard],  // Cualquiera que esté autenticado puede acceder
+    data: { roles: ['CLIENTE', 'VETERINARIO', 'ADMIN', 'ADMIN_CLINICA'] } */
+  },
 ];
 
 @NgModule({
