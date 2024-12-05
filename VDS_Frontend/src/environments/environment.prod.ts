2048feab8332b411834fc0ec5e1256774cab8eb9
Sergio Forja Barriga <sergio.forja88@gmail.com>
2024-12-05 23:22:28 +0100

    Nueva URL de backend en producción

--- VDS_Frontend/src/environments/environment.prod.ts
+++ VDS_Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
-    APIEndpoint: 'https://vetdatashare.onrender.com/api/',
-    APIHost: 'https://vetdatashare.onrender.com/',
+    /* APIEndpoint: 'https://vetdatashare.onrender.com/api/',
+    APIHost: 'https://vetdatashare.onrender.com/', */
+    APIEndpoint: 'https://vetdatashare-backend.onrender.com/api/',
+    APIHost: 'https://vetdatashare-backend.onrender.com/',
 };
