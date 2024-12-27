5d1d72614a7d8eaaf71668c233c522adb122f518
Sergio Forja Barriga <sergio.forja88@gmail.com>
2024-12-27 01:24:35 +0100

    Se realiza página de portada incluyendo slideshow con mat tabs

--- VDS_Frontend/src/app/Components/home/home.component.ts
+++ VDS_Frontend/src/app/Components/home/home.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  images = [
+    'assets/images/slideshow1.jpg',
+    'assets/images/slideshow2.jpg',
+    'assets/images/slideshow3.jpg'
+  ];
 
+  activeIndex = 0;
+  intervalId: any;
+
+  ngOnInit(): void {
+    this.startSlideshow();
+  }
+
+  startSlideshow(): void {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 5000); // Cambia cada 5 segundos
+  }
+
+  nextSlide(): void {
+    this.activeIndex = (this.activeIndex + 1) % this.images.length;
+  }
+
+  goToSlide(index: number): void {
+    this.activeIndex = index;
+    this.resetSlideshow(); // Reinicia el temporizador
+  }
+
+  resetSlideshow(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+    this.startSlideshow();
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
--- VDS_Frontend/src/app/app.module.ts
+++ VDS_Frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './Components/home/home.component';
 import { AuthInterceptor } from './auth/Services/auth-interceptor.service';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({ declarations: [
         AppComponent,
@@ -29,7 +30,9 @@ import { AuthInterceptor } from './auth/Services/auth-interceptor.service';
         MatFormFieldModule,
         MatButtonModule,
         MatSelectModule,
-        MatSnackBarModule], 
+        MatSnackBarModule,
+        MatTabsModule 
+    ], 
     providers: [
         {
             provide: HTTP_INTERCEPTORS,
